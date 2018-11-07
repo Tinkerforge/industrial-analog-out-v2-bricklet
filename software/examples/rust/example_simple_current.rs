4ffdb45f2a3dcbd226afffdab940f154585b4b59
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:21:47 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple_current.rs
+++ software/examples/rust/example_simple_current.rs
@@ -1,26 +1,26 @@
 use std::{error::Error, io};
 
-use tinkerforge::{industrial_analog_out_v2_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_analog_out_v2_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Analog Out Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Analog Out Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_analog_out_v2_bricklet = IndustrialAnalogOutV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let iao = IndustrialAnalogOutV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Set output current to 4.5mA
-    industrial_analog_out_v2_bricklet.set_current(4500);
-    industrial_analog_out_v2_bricklet.set_enabled(true);
+    iao.set_current(4500);
+    iao.set_enabled(true);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    industrial_analog_out_v2_bricklet.set_enabled(false);
+    iao.set_enabled(false);
     ipcon.disconnect();
     Ok(())
 }
