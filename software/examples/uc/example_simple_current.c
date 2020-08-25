3e21dfb418d5524de5b3d054ad550f4707b0f0f7
Erik Fleckstein <erik@tinkerforge.com>
2020-08-25 15:52:27 +0200

    uc: Fix whitespace

--- software/examples/uc/example_simple_current.c
+++ software/examples/uc/example_simple_current.c
@@ -1,14 +1,9 @@
-
-
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_industrial_analog_out_v2.h"
 
 #define UID "XYZ" // Change XYZ to the UID of your Industrial Analog Out Bricklet 2.0
 
 void check(int rc, const char* msg);
-
-
-
 
 TF_IndustrialAnalogOutV2 iao;
 
