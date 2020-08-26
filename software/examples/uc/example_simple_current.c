7b8f11a9698b156ede56555e3839e3eed2cbb610
Erik Fleckstein <erik@tinkerforge.com>
2020-08-26 10:36:02 +0200

    uc: Fix whitespace

--- software/examples/uc/example_simple_current.c
+++ software/examples/uc/example_simple_current.c
@@ -11,11 +11,9 @@ void example_setup(TF_HalContext *hal) {
 	// Create device object
 	check(tf_industrial_analog_out_v2_create(&iao, UID, hal), "create device object");
 
-
 	// Set output current to 4.5mA
 	check(tf_industrial_analog_out_v2_set_current(&iao, 4500), "call set_current");
 	check(tf_industrial_analog_out_v2_set_enabled(&iao, true), "call set_enabled");
-
 }
 
 void example_loop(TF_HalContext *hal) {
--- software/examples/uc/example_simple_voltage.c
+++ software/examples/uc/example_simple_voltage.c
@@ -11,11 +11,9 @@ void example_setup(TF_HalContext *hal) {
 	// Create device object
 	check(tf_industrial_analog_out_v2_create(&iao, UID, hal), "create device object");
 
-
 	// Set output voltage to 3.3V
 	check(tf_industrial_analog_out_v2_set_voltage(&iao, 3300), "call set_voltage");
 	check(tf_industrial_analog_out_v2_set_enabled(&iao, true), "call set_enabled");
-
 }
 
 void example_loop(TF_HalContext *hal) {
