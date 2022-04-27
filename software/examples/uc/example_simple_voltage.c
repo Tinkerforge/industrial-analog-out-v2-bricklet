2693f0382f2777bf8f060d18f5e857e12cfab57e
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 11:48:15 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_simple_voltage.c
+++ software/examples/uc/example_simple_voltage.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_industrial_analog_out_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_industrial_analog_out_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Industrial Analog Out Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_IndustrialAnalogOutV2 iao;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_industrial_analog_out_v2_create(&iao, UID, hal), "create device object");
+	check(tf_industrial_analog_out_v2_create(&iao, NULL, hal), "create device object");
 
 	// Set output voltage to 3.3V
 	check(tf_industrial_analog_out_v2_set_voltage(&iao, 3300), "call set_voltage");
